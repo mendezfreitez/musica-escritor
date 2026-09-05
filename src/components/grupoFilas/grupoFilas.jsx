9d310a7ed9145d416fdd517ee688c7d88cf4a41b
Daniel Mendez <mendezfreitez@gmail.com>
2026-09-04 23:30:27 -0400

    - Mejoras

--- src/components/grupoFilas/grupoFilas.jsx
+++ src/components/grupoFilas/grupoFilas.jsx
@@ -5,15 +5,22 @@ export const GrupoFilas = ({ filas, editar, guardar }) => {
 
 
     return (
-        <div>
+        <div className="w-[90%]">
             {filas.map(el => {
                 const { id, texto, edit } = el;
                 return (
                     <>
                         {edit === 0
-                            ? <p className="text-[#464646]" style={{fontSize: "20px", lineHeight: "40px"}} onClick={() => { editar(el) }} key={id}>{texto}</p>
+                            ?
+                            <>
+                                <div className="h-[40px] w-full bg-cyan-600">
+                                </div>
+                                <p className="text-[#464646]" style={{ fontSize: "20px", lineHeight: "40px" }} onClick={() => { editar(el) }} key={id}>
+                                    {texto}
+                                </p>
+                            </>
                             : <InputFila
-                                clases="rounded-md ps-1 text-[#464646] bg-gray-200 border-0 focus-visible:outline-0 focus-visible:inset-shadow-sm"
+                                clases="rounded-md ps-1 text-[#464646] bg-gray-200 border-0 focus-visible:outline-0 focus-visible:inset-shadow-sm d-flex w-full"
                                 texto={texto}
                                 ejecutar={guardar}
                             />
